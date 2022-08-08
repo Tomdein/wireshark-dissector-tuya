b9e14af67c06faede2ac69d9aa2d97ea84a65b2c
Tomas Deingruber <Deingruber.Tomas@gmail.com>
2022-08-08 15:20:07 +0200

    Add code info and description

--- tuya.lua
+++ tuya.lua
@@ -7,6 +7,91 @@ local AES128Cipher = require("lockbox.cipher.aes128");
 local ZeroPadding = require("lockbox.padding.zero");
 local Stream = require("lockbox.util.stream");
 
+-- Some info:
+    -- Right now, this dissector only supports protocol version 3.4, which is still W.I.P
+    -- To use this dissector you have to copy lockbox lib to the right directory (I did to 'C:\Program Files\Wireshark\lua')
+        -- Download the lockbox lib from https://github.com/somesocks/lua-lockbox
+        -- Take all the files from lua-lockbox/lockbox and copy them to 'C:\Program Files\Wireshark\lua' (i.e. init.lua, cipher folder, ... are in 'C:\Program Files\Wireshark\lua\init.lua', 'C:\Program Files\Wireshark\lua\cipher\')
+        -- Take this script (tuya.lua) and place it (or symlink it from git folder) to 'C:\Program Files\Wireshark\plugins\3.6' or one of the lua plugins folder (Help -> About Wireshark -> Folders)
+    -- You have to input device_key into the protocol settings (right click on Tuya packet -> Protocol Preferences -> Tuya Protocol -> device_key)
+        -- You need to get the device_key from tuya developer cloud site (https://iot.tuya.com/, guide at for example: https://github.com/rospogrigio/localtuya)
+    -- The device key is in ASCII string, while others are in hex stream - e.g. B61978 == 0xB6 0x19 0x78 (because they have random bytes and values 0x00-0x1F fuck up the printing... I am in LUA what can I do, pls help :D)
+    -- I do not know where does the naming of the commands came from, so it may not be relevant ¯\_(ツ)_/¯
+    -- If the decoding is screwed up, click packets in this order: BIND(0x03), RENAME_GW(0x04)
+
+    -- If any of this info seem broken, go to first commit of this info: 'Add code info and description'
+
+    -- TERMINOLOGY
+    -- Some terminology (may not match with the terminology of https://github.com/harryzz/tuyapi):
+        -- local == pc/mobile
+        -- remote == Tuya device (switch, sensor, ...)
+        -- header == first 16B (B == bytes)
+        -- payload == all data after header 
+        -- return code == only sometimes! Only from remote? 4B after header counts to payload_len
+        -- dps == datapoints
+
+    -- PACKET DESCRIPTION
+    -- Sometimes there are multiple tuya packets inside one tcp!!!!
+    -- The Tuya header and payload:
+        -- Header (16B):
+            -- 4B Prefix 0x000055AA
+            -- 4B Sequence_number (separate sequence numbers for remote and local)
+            -- 4B Command
+            -- 4B Payload length
+        
+        -- Payload (Payload length long):
+            -- Sometimes 4B return_code from remote
+            -- xB DATA - has padding
+                -- As to my understanding from the code I am rewriting, the padding is always there. Not shure it is true all the time.
+                -- Padding bytes are the length of padding (0x........10101010101010101010101010101010 - padding is 0x10 == 16 and is 16B long)
+            -- 32B HMAC_SHA256 - for as error detectiong code (crc/hash....)
+            -- 4B suffix 0x0000AA55
+
+        -- DATA is always ECB_AES128 encrypted with device_key
+
+    -- HANDSHAKE AND SESSION_KEY GENERATION
+    -- This is how the session_key handshake goes (after TCP Syn handshake):
+        -- local generates local_key == 16B (B == bytes) random
+        -- add padding to local_key (0x10101010101010101010101010101010 when (len % 16 == 0))
+        -- data = encode local_key using ecb_aes128 with device_key as passw (no padding or iv)
+        -- create packet with prefix, local_seq_n, command = 0x00000003 (BIND), len (68? calculate it yourself :D), data
+        -- calculate HMAC_SHA256 of previous packet using device_key as secret (passw) (no padding or iv)
+        -- append hmac_hash to packet and append suffix (0x0000AA55)
+        -- send!
+
+        -- receive tuya packet (payload_len = 104B) with command 0x04 (do the previous in reverse :) ):
+        -- remove suffix
+        -- read the hmac_hash, calculate the hash (using device_key as secret) of the rest yourself and compare -> check if not data is lost/manipulated
+        -- remove return_code from data in payload (first 4B in payload) if present (look at code at 'Remove return code' in tuya_protocol.dissector(...))
+        -- take the remaining data in payload and decrypt it using ecb_aes128 with device_key as passw:
+            -- remove padding (look at code at 'Remove padding' in _decrypt())
+            -- first 16B is the remote_key
+            -- next 32B is the HMAC_SHA256 hash of the local_key using device_key as secret (passw) -> calculate it yourself and compare
+        
+        -- calculate the session_key as (python code):
+            -- XOR coresponding bytes and then encrypt using device_key as passw
+
+            -- for i in range(0x00, 0x10):
+            --     session_key.append(local_key[i] ^ remote_key[i])
+
+            -- session_key = cipher_ECB_AES128.encrypt(session_key)
+            
+        -- send the message similar to the first one (0x00000003, BIND)
+            -- increment local seq_n
+            -- command = 0x00000005 (RENAME_DEVICE)
+            -- len = 84
+            -- Data = HMAC_SHA256 of remote_key using device_key as secret (passw)
+
+        -- Ta duh... You are connected :)
+
+
+    -- ADDITIONAL INFO
+    -- for more info on the 3.4 protocol and where I 'inspired' myself go to:
+        -- encryption described (mainly looked at code in repo below :D): https://github.com/codetheweb/tuyapi/issues/481#issuecomment-921756711
+        -- harryzz tuyapi repo with 3.4 implementation: https://github.com/harryzz/tuyapi
+        -- codetheweb original tuyapi: https://github.com/codetheweb/tuyapi
+
+
 function _encrypt(key, data)
     local cipher = ECBMode.Cipher()
         .setKey(Array.fromString(key))
@@ -33,8 +118,11 @@ function _decrypt(key, data)
         .asBytes();
     
     print("\n\nKey : " .. key .. ", \ndata : '" .. Array.toHex(Array.fromString(data)) .. "', \ndecrypted_data : '" .. Array.toHex(decrypted_bytes) .. "'\n")
+    
+    -- Remove padding
     decrypted_bytes_len = Array.size(decrypted_bytes)
-    decrypted_bytes = Array.truncate(decrypted_bytes, decrypted_bytes_len - decrypted_bytes[decrypted_bytes_len])
+    decrypted_bytes = Array.truncate(decrypted_bytes, decrypted_bytes_len - decrypted_bytes[decrypted_bytes_len]) 
+    
     print("\n\nKey : " .. key .. ", \ndata : '" .. Array.toHex(Array.fromString(data)) .. "', \ndecrypted_data : '" .. Array.toHex(decrypted_bytes) .. "'\n")
     return Array.toHex(decrypted_bytes)
 end
@@ -151,6 +239,8 @@ function tuya_protocol.dissector(buffer, pinfo, tree)
         tuya_states[ip_id] = "other"
     end
 
+    -- Remove return code
+    -- If there is a return value, remove it (Return value is from the remote)
     local payload = nil
     if(bit.band(buffer(HEADER_SIZE, 4):uint(), 0xFFFFFF00) ~= 0) then
         payload = buffer(HEADER_SIZE, payload_size:uint() - 0x24):bytes():raw()
@@ -168,13 +258,11 @@ function tuya_protocol.dissector(buffer, pinfo, tree)
         remote_key = string.sub(decrypted_payload, 1, 32) -- 1B is 2 chars -> 16B are 32 chars
         session_key = Array.toString(Array.XOR(Array.fromHex(local_key), Array.fromHex(remote_key)))
         session_key = _encrypt(tuya_protocol.prefs.device_key, session_key)
+    elseif tuya_states[ip_id] == "else" and command == 0x5 then
+        decrypted_payload = _decrypt(tuya_protocol.prefs.device_key, payload)
     else
-        decrypted_payload = decrypt(payload)
+        decrypted_payload = Array.toString(Array.fromHex(decrypt(payload)))
     end
-
-    print("payload: " .. payload)
-    print("decrypted data: " .. decrypted_payload)
-    print("device_key: " .. tuya_protocol.prefs.device_key)
 
     if packet_no == 1 then
             tuya_subtree:add(pf_device_key, tuya_protocol.prefs.device_key)
